b54fcdccbced53c938e6286e94afd52944da0152
raghavlakh <raghavlakhotia25@gmail.com>
2023-02-02 10:35:22 +0530

    update

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,10 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
 
-})(window);
+}) (window);
